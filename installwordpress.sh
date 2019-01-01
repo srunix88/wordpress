9651add87948e1f926df54887f830c667276ab30
Clinton Fisher <32957231+srunix88@users.noreply.github.com>
2019-01-01 14:03:07 -0500

    Initial Draft of full wordpress install
    Install LAMP, wordpress, and virtual hosts on new Ubuntu server

---
+++ installwordpress.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# WordPress Install 
+# Ubuntu 16_04 install from scratch
+# Run this script as root or with sudo.
+
+DomainsRootHome=/home
+WPDIR=public_html
+Domain=example.com
+WP_USERS="user1wp user2wp user3wp user4wp"
+WP_DB=examplewp
+
+if ! host -t a google.com 
+then 
+cat > /etc/resolv.conf << EOF
+domain srunix.net
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+EOF
+
+apt-get update 
+apt install tasksel
+tasksel install lamp-server
+apt install curl
+
+apt install php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+
+mv wp-cli-phar /usr/local/bin/wp
+WordPress_LOCATION='http://wordpress.org/latest.tar.gz'
+wget --no-check-certificate $WordPress_LOCATION -O /tmp/wordpress.tar.gz
+tar -xz -C /tmp -f /tmp/wordpress.tar.gz
+
+for User in $WP_USERS
+do
+   TARGET=$DomainsRootHome/$USER/$WPDIR
+   mkdir -p $TARGET
+   cp -r /tmp/wordpress $TARGET
+   chown -R www-data:www-data $TARGET
+   vhost-setup $USER
+done
+
+vhost-setup () {
+
+echo "Not yet implemented - need to do it manually"
+
+}
