a94a9797f022e76b58d53886b68c780c091dffb5
Clinton Fisher <32957231+srunix88@users.noreply.github.com>
2019-01-02 10:31:58 -0500

    Added more bug fixes.
    Tweaking a bit more. Adding mysql database and configs for wp. This may not work by running as root - the sudo command with a function is a guess. It kinda runs from the command line - but if this does not work, I can at least remember the commands I ran manually.

--- installwordpress.sh
+++ installwordpress.sh
@@ -6,7 +6,7 @@
 DomainsRootHome=/home
 WPDIR=public_html
 Domain=example.com
-WP_USERS="user1 user2 user3 user4"
+USERS="user1 user2 user3 user4"
 WP_DB=examplewp
 DB_PW=password
 
@@ -22,6 +22,14 @@ fi
 apt-get update 
 apt install tasksel
 tasksel install lamp-server
+### this failed on Ubuntu - need to see if there is a fix.
+### also need to set the root password for mysql and secure it.
+### TODO
+#mysql << EOF
+#ALTER USER 'root'@'localhost' IDENTIFIED by 'password';
+#EOF
+
+
 apt install curl
 
 apt install php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc
@@ -29,21 +37,30 @@ apt install php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 
-mv wp-cli-phar /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/wp
 WordPress_LOCATION='http://wordpress.org/latest.tar.gz'
 wget --no-check-certificate $WordPress_LOCATION -O /tmp/wordpress.tar.gz
 tar -xz -C /tmp -f /tmp/wordpress.tar.gz
+mv /tmp/wordpress /tmp/public_html
 
-useradd -u 33 -g 33 -s /bin/bash -d /home/ncfisher -m ncfisher
+useradd -u 555 -g 33 -s /bin/bash -d /home/ncfisher -m ncfisher
 
-for User in $WP_USERS
+for User in $USERS
 do
-   TARGET=$DomainsRootHome/$User/$WPDIR
+   TARGET=$DomainsRootHome/$User
    mkdir -p $TARGET
-   cp -r /tmp/wordpress $TARGET
+   cp -r /tmp/public_html $TARGET
    chown -R www-data:www-data $TARGET
-   wp core config --dbname=${User}wp --dbuser=${User}wp --dbpass=$DB_PW --dbhost=localhost --dbprefix=wp_
-   vhost-setup $Domain $User $TARGET
+   cd $TARGET/public_html
+   mysql << EOF
+   create user '${User}wp'@'localhost' IDENTIFIED by 'password';
+   create database ${User}wp
+   GRANT ALL PRIVILEGES ON ${User}wp TO '${User}wp'@'localhost';
+   FLUSH PRIVILEGES;
+EOF
+   
+   sudo -u ncfisher -i -- configwp
+   #vhost-setup $Domain $User $TARGET
 done
 
 vhost-setup () {
@@ -55,5 +72,13 @@ host_dir=$3
 wget -O virtualhost https://raw.githubusercontent.com/RoverWire/virtualhost/master/virtualhost.sh
 chmod +x virtualhost
 bash virtualhost create $subdomain.$Domain $host_dir
+
+}
+
+configwp () {
+
+cd $DomainsRootHome/$User
+pwd
+wp core config --dbname=${User}wp --dbuser=${User}wp --dbpass=$DB_PW --dbhost=localhost --dbprefix=wp_
 
 }
